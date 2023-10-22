406eafee60044c7e1634cc9850f7d17e3cbed4fd
yusra <yousrabouhraoua63@gmail.com>
2023-10-22 19:59:28 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+const App =()=>(
+  <main className="relative ">
+    Nav
+    <section className="x1:padding-1 wide:padding-r padding-b">
+
+    </section>
+    <section className="padding">
+      Popularproducts
+    </section>
+    <section className="padding">
+      superquality
+    </section>
+    <section className="padding-x py-10">
+      servicies
+    </section>
+    <section className="padding">
+      specialoffer
+    </section>
+    <section className="bg-pale-blue padding">
+      customerReviwes
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      subscribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      footer
+    </section>
+  </main>
+);
+
+export default App;
